5568c285f40f517eb41766fe327e0ccde2944610
Arpan <arpansinghrajput123@gmail.com>
2026-04-21 20:41:01 +0530

    updaetd

--- frontend/src/utils/serverUrl.js
+++ frontend/src/utils/serverUrl.js
@@ -1,6 +1,6 @@
 let serverUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/"
-    : "http://localhost:5000/";
+    : "http://13.235.80.109:5000/";
 
 export default serverUrl;
