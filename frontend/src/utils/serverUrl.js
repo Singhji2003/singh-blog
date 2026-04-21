849701ca171ebb1c0d73b78e95374ac10e4f3812
Arpan <arpansinghrajput123@gmail.com>
2026-04-21 20:49:25 +0530

    updadtw

--- frontend/src/utils/serverUrl.js
+++ frontend/src/utils/serverUrl.js
@@ -1,4 +1,5 @@
-let serverUrl =
+const serverUrl =
+  typeof window !== "undefined" &&
   window.location.hostname === "localhost"
     ? "http://localhost:5000/"
     : "http://13.235.80.109:5000/";
