e505567f680625e2d975fd1009181abbc4b48e89
Ubuntu <ubuntu@ip-172-31-5-249.ap-south-1.compute.internal>
2026-04-22 08:29:24 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,10 @@ npm install
 echo "Building frontend..."
 cd ../frontend
 npm install
+rm -rf .next
 npm run build
-
+pm2 restart frontend
 echo "Restarting backend..."
+cd ../backend
 pm2 restart index
 echo "Done!"
