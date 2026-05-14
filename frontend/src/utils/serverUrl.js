b0652bea640003d8c055890048bf7071d97ff285
Arpan <arpansinghrajput123@gmail.com>
2026-05-14 17:42:58 +0530

    updaetd

--- frontend/src/utils/serverUrl.js
+++ frontend/src/utils/serverUrl.js
@@ -1,6 +1,6 @@
 const serverUrl =
   typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://localhost:5000/"
-    : "http://api.featherbazaar.com/";
+    : "https://api.featherbazaar.com/";
 
 export default serverUrl;
