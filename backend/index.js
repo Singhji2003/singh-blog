a8d2800ea2753bb66794ccee6c941983dfab4b0b
Arpan <arpansinghrajput123@gmail.com>
2026-05-14 17:42:04 +0530

    updaetd

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ import CacheService from "./services/cache.service.js";
 
 // Initialize app
 const app = express();
+app.set("trust proxy", true);
 dotenv.config();
 
 try {
@@ -32,6 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // health check
 app.get("/health", (req, res) => {
+  console.log("HOST:", req.headers.host);
   return res.status(200).json({ msg: "Server is Running!! 🎉 " });
 });
 
--- frontend/src/utils/serverUrl.js
+++ frontend/src/utils/serverUrl.js
@@ -1,6 +1,6 @@
 const serverUrl =
   typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://localhost:5000/"
-    : "https://api.featherbazaar.com/";
+    : "http://api.featherbazaar.com/";
 
 export default serverUrl;
