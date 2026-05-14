b2ac3766b80c4c1918f92f3f5f0f76a186a9c4c7
Arpan <arpansinghrajput123@gmail.com>
2026-05-14 17:07:06 +0530

    udpaetd

--- frontend/src/utils/serverUrl.js
+++ frontend/src/utils/serverUrl.js
@@ -1,6 +1,6 @@
 const serverUrl =
   typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://localhost:5000/"
-    : "http://singh-blog-lb-733786724.ap-south-1.elb.amazonaws.com/";
+    : "https://api.featherbazaar.com/";
 
 export default serverUrl;
